1da33fb9d98f0c7e1b3a80162e1eb99e867e36a6
haymandan <109345327+haymandan@users.noreply.github.com>
2022-11-12 14:51:43 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.deepsoundscript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | DeepSound - The Ultimate PHP Sound Sharing Platform
+// | Copyright (c) 2019 DeepSound. All rights reserved.
+// +------------------------------------------------------------------------+
+header("Location: ./install");
+exit();
+// MySQL Hostname
+$sql_db_host = "sql112.epizy.com";
+// MySQL Database User
+$sql_db_user = "epiz_32852059";
+// MySQL Database Password
+$sql_db_pass = "p5BJenHc9n5i";
+// MySQL Database Name
+$sql_db_name = "epiz_32852059_Sound";
+
+// Site URL
+$site_url = "http://spece.ml"; // e.g (http://example.com)
+
+$developer_mode = 0;
+
+// Purchase code
+$purchase_code = ""; // Your purchase code, don't give it to anyone.
+?>
